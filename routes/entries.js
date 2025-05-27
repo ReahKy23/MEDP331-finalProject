a4f4844875b009a7b48c0a230866d77227571c19
ReahKy23 <Kylah.kerr23@gmail.com>
2025-05-27 08:48:34 -0400

    updated
    this has the addition professor recommended to host my site on glitch

--- routes/entries.js
+++ routes/entries.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { getDb } = require('../config/db');
+const db = require('../config/db');
 
-// GET /entries
-router.get('/', async (req, res) => {
-  const db = getDb();
-  const entries = await db.collection('entries').find({}).toArray();
-  res.render('entries', { entries });
+router.post('/', async (req, res) => {
+  try {
+    const { name, birthYear, deathYear, description } = req.body;
+
+    if (!name || !birthYear || !deathYear || !description) {
+      return res.status(400).send('All fields are required');
+    }
+
+    const database = db.getDb();
+    const collection = database.collection('entries');
+
+    await collection.insertOne({ name, birthYear, deathYear, description });
+
+    res.redirect('/entries');
+  } catch (err) {
+    console.error('POST /entries error:', err);
+    res.status(500).send('Error saving entry');
+  }
 });
 
+router.get('/', async (req, res) => {
+  try {
+    const database = db.getDb();
+    const collection = database.collection('entries');
+    const entries = await collection.find().toArray();
+
+    res.render('entries', { entries });
+  } catch (err) {
+    console.error('GET /entries error:', err);
+    res.status(500).send('Error loading entries');
+  }
+});
 
 module.exports = router;
